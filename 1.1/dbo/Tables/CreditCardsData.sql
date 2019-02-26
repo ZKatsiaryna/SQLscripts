5f0f55cd88e7fe438ea2b18b7935ed71dbad0260
Kate <katya.zhaunerchyk@gmail.com>
2019-02-27 00:35:35 +0300

    add task 3.2

---
+++ 1.1/dbo/Tables/CreditCardsData.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CreditCardsData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CardNumber] INT NOT NULL, 
+    [ExpirationDate] DATETIME2 NOT NULL, 
+    [CardHolder] NCHAR(10) NULL, 
+    [EmployeeID] INT NULL, 
+    CONSTRAINT [FK_CreditCardsData_EmployeeID] FOREIGN KEY (EmployeeID) REFERENCES [Employees](EmployeeID)
+)
