55eecb4b95a637c6e96b25ff4f46e04d3a1f7ad6
Kate <katya.zhaunerchyk@gmail.com>
2019-03-03 17:01:00 +0300

    Add post deploy script

---
+++ NorthwindPostDeploy/NorthwindPostDeploy/dbo/Views/CreditCardsData.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CreditCardsData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CardNumber] INT NOT NULL, 
+    [ExpirationDate] DATETIME NOT NULL, 
+    [CardHolder] NCHAR(10) NULL, 
+    [EmployeeID] INT NULL, 
+    CONSTRAINT [FK_CreditCardsData_Employee] FOREIGN KEY (EmployeeID) REFERENCES [Employees](EmployeeID)
+)
